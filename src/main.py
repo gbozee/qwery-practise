e3973d1ecf64a046fe9ef07c67a74bac7ecbc4c8
tmosco <tmosco_tayo@yahoo.com>
2017-06-24 16:46:56 +0100

    View

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/home')
-def home():
-    return 'Hello'
+def result():
+    todos= [
+        {"ID": 1,"title": "Want to go out", "uncompleted":False},
+        {"ID": 2,"title": "Go to the shop", "uncompleted":False},
+        {"ID": 3,"title": "Watch Movies", "uncompleted":False} ]
+    return render_template("home.html",todos=todos)
+
+
