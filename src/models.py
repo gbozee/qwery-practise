486c767b1a332fdde32332a20929fa238222c5bc
Obiamaka Agbaneje <obiamaka.agbaneje@gmail.com>
2017-06-24 14:13:53 +0100

    Add SaveMixin

--- src/models.py
+++ src/models.py
@@ -2,7 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class TodoList(db.Model):
+class SaveMixin(object):
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+class TodoList(SaveMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     status = db.Column(db.Boolean)
