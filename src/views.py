7e14cd1b89d05998bfe6bed4b7f9cbdecc0f7103
Obiamaka Agbaneje <obiamaka.agbaneje@gmail.com>
2017-07-01 16:53:12 +0100

    Add Edit Todo Route

--- src/views.py
+++ src/views.py
@@ -18,3 +18,11 @@ def add_todo():
             return redirect('/home')
     return render_template('add_todo.html', form=form)
     
+@app.route('/todo/<int:todo_id>', methods=['POST', 'GET'])
+def edit_todo(todo_id):
+    todo = TodoList.query.get(todo_id)
+    todo_form = TodoForm(obj=todo)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            return redirect('/home')
+    return render_template('todo.html', form=todo_form, todo=todo)
