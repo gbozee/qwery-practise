6c1aa445c2005d01fcbc6177e5a6bdf7a8f916cf
oluwayemisi <oluwayemisimolade@gmail.com>
2017-07-12 14:03:29 +0100

    Addition of login and logout views

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
@@ -17,7 +18,19 @@ class TodoList(SaveMixin,db.Model):
         return "<TodoList {}>".format(self.title)
 
     @classmethod
-    def create(cls,**kwargs):
+    def create(cls, **kwargs):
         new_data = TodoList(**kwargs)
         new_data.save()
         return new_data
+
+class User(UserMixin, SaveMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String())
+    email = db.Column(db.String(70))
+    password = db.Column(db.String(70))
+
+    @classmethod
+    def create(cls, **kwargs):
+        new_user = User(**kwargs)
+        new_user.save()
+        return new_user
--- src/views.py
+++ src/views.py
@@ -1,18 +1,30 @@
-from flask import Flask,render_template,redirect,request
+from flask import Flask, flash, render_template, redirect, request, session, url_for
+from flask_login import LoginManager, login_required, login_user, logout_user
 from src import app
-from .forms import TodoForm
+from .models import User
+from .forms import TodoForm, LoginForm, SignupForm
+
+login_manager = LoginManager()
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter(User.id == user_id).first()
+
 
 @app.route('/home')
 def home():
-    todos= [
-        {"title": "Want to go out", "date": "June 21st, 2017","completed":False},
-        {"title": "Go to the shop", "date": "June 21st, 2017","completed":True},
-        {"title": "Watch Movies", "date": "June 21st, 2017","completed":False}
+    todos = [
+        {"title": "Want to go out", "date": "June 21st, 2017", "completed": False},
+        {"title": "Go to the shop", "date": "June 21st, 2017", "completed": True},
+        {"title": "Watch Movies", "date": "June 21st, 2017", "completed": False}
     ]
-    return render_template("home.html",todos=todos)
+    return render_template("home.html", todos=todos)
 
 
 @app.route('/add_todo', methods=['POST', 'GET'])
+@login_required
 def add_todo():
     form = TodoForm()
     if request.method == 'POST':
@@ -20,4 +32,33 @@ def add_todo():
             form.save()
             return redirect('/home')
     return render_template('add_todo.html', form=form)
-    
+
+
+@app.route("/login/", methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = form.get_user()
+        login_user(user)
+        flash('Logged in successfully.', "success")
+        next_ = request.args.get('next')
+
+        return redirect(next_ or url_for('home'))
+    return render_template('login.html', form=form)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("You have successfully logged out.", "info")
+    return redirect(url_for('home'))
+
+
+@app.route("/signup/", methods=['POST', 'GET'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        form.save()
+        return redirect('/login')
+    return render_template('signup.html', form=form)
